6a0b6d78a986fc833c466732291e18e5d62cfcfa
thamndex-star <thamndex@gmail.com>
2026-03-15 22:27:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ console.log("debug")
 eval("2+2")
 
 const { exec } = require("child_process")
+const API_KEY = "123456789abcdefg123456"
+
+const password = "supersecretpassword"
+
+console.log("debug")
+
+eval("2+2")
+
+const { exec } = require("child_process")
